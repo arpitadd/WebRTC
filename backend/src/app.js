f4bb826ae138df8db6b97a1463c915bf697fcaf4
Arpit jha <arpitjha7878@gmail.com>
2026-01-09 01:12:52 +0530

    Backend setup

---
+++ backend/src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import { createServer } from "node:http";
+import dotenv from "dotenv";
+dotenv.config();
+import connectToSocket from "./controllers/socketManager.js";
+
+import { Server } from "socket.io";
+import cors from "cors";
+import mongoose from "mongoose";
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+const start = async () => {
+  try {
+    const connectionDb = await mongoose.connect(process.env.MONGO_URL);
+
+    console.log(
+      `MongoDB connected: ${connectionDb.connection.host}`
+    );
+
+    server.listen(app.get("port"), () => {
+      console.log(`Listening on port ${app.get("port")}`);
+    });
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+  }
+};
+
+start();
