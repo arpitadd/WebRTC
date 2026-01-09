b5e4bfbbbc61a4bd2936c6fb42613f835a491927
Arpit jha <arpitjha7878@gmail.com>
2026-01-10 00:03:10 +0530

    Created authentication routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import { createServer } from "node:http";
 import dotenv from "dotenv";
 dotenv.config();
 import connectToSocket from "./controllers/socketManager.js";
+import userRoutes from "./routes/users.routes.js";
 
 import { Server } from "socket.io";
 import cors from "cors";
@@ -14,6 +15,7 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use("/api/v1/users", userRoutes);
 const start = async () => {
   try {
     const connectionDb = await mongoose.connect(process.env.MONGO_URL);
