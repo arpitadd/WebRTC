b60f3b94e1a3f80ba7073a593e2f7d90c848d80e
Arpit jha <arpitjha7878@gmail.com>
2026-01-11 00:46:58 +0530

    setting up the router

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LandingPage from './pages/landing';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <Router>
+<Routes>
+    <Route path='/' element={<LandingPage />} />
+
+
+</Routes>
+
+    </Router>
+    
+    </>
+  );
+}
